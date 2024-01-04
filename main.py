df4e7f5893026dcb04cbbcdeeafd49b09224fbb7
DImiir <d.i.mironenko@yandex.ru>
2024-01-04 17:36:13 +0300

    у него теперь могут быть смертные друзья :D

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 all_sprites = pygame.sprite.Group()
+attacks = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
 info = pygame.display.Info()
 size = width, height = 0.8 * info.current_w, 0.8 * info.current_h
 screen = pygame.display.set_mode(size)
@@ -13,90 +16,110 @@ clock = pygame.time.Clock()
 class Character(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(all_sprites)
-        first_pos = pygame.image.load('pictures/character/1pos.tiff')
+        self.first_pos = pygame.image.load('pictures/character/1pos.tiff')
         self.image = pygame.transform.scale(
-            first_pos, (first_pos.get_width() * 4,
-                        first_pos.get_height() * 4))
-        self.frames_for_right = [pygame.image.load('pictures/character/go_right2.tiff'),
+            self.first_pos, (self.first_pos.get_width() * 4,
+                             self.first_pos.get_height() * 4))
+        self.frames_for_right = [pygame.image.load('pictures/character/go_right1.tiff'),
                                  pygame.image.load('pictures/character/go_right1.tiff'),
                                  pygame.image.load('pictures/character/go_right2.tiff'),
+                                 pygame.image.load('pictures/character/go_right2.tiff'),
+                                 pygame.image.load('pictures/character/go_right3.tiff'),
                                  pygame.image.load('pictures/character/go_right3.tiff')]
         self.frames_for_left = [pygame.transform.flip(im, True, False) for im in self.frames_for_right]
         self.frames_for_up = [pygame.image.load('pictures/character/go_back1.tiff'),
+                              pygame.image.load('pictures/character/go_back1.tiff'),
                               pygame.image.load('pictures/character/go_back2.tiff'),
+                              pygame.image.load('pictures/character/go_back2.tiff'),
+                              pygame.image.load('pictures/character/go_back3.tiff'),
                               pygame.image.load('pictures/character/go_back3.tiff')]
-        self.right = True
-        self.cur_frame = 0
+        self.right, self.up = True, False
+        self.cur_frame1, self.cur_frame2, self.cur_frame3, self.cur_frame4 = 0, 0, 0, 0
         self.speed = 10
         self.rect = self.image.get_rect().move(pos_x, pos_y)
         self.pos_x = pos_x
         self.pos_y = pos_y
 
-    def move(self, where):
-        if where == 'up':
+    def move(self):
+        if to_up1:
             if self.pos_y > 35:
                 self.pos_y -= self.speed
-        if where == 'down':
-            if self.pos_y < height - self.image.get_height() - 16:
+        if to_down1:
+            if self.pos_y < height - self.image.get_height() - 15:
                 self.pos_y += self.speed
-        if where == 'left':
-            if self.pos_x > 16:
+        if to_left1:
+            if self.pos_x > 15:
                 self.pos_x -= self.speed
-        if where == 'right':
-            if self.pos_x < width - self.image.get_width() - 16:
+        if to_right1:
+            if self.pos_x < width - self.image.get_width() - 15:
                 self.pos_x += self.speed
         self.rect = self.image.get_rect().move(self.pos_x, self.pos_y)
 
     def update(self):
-        if to_up1:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_up)
-            image = self.frames_for_up[self.cur_frame]
-            pygame.time.delay(10)
+        if not any([to_up1, to_down1, to_left1, to_right1]):
+            if self.up:
+                self.image = pygame.transform.scale(
+                    self.frames_for_up[0], (self.frames_for_up[0].get_width() * 4,
+                                            self.frames_for_up[0].get_height() * 4))
+            elif self.right:
+                self.image = pygame.transform.scale(
+                    self.first_pos, (self.first_pos.get_width() * 4,
+                                     self.first_pos.get_height() * 4))
+            else:
+                self.image = pygame.transform.scale(
+                    self.first_pos, (self.first_pos.get_width() * 4,
+                                     self.first_pos.get_height() * 4))
+                self.image = pygame.transform.flip(self.image, True, False)
+        elif to_up1:
+            self.cur_frame1 = (self.cur_frame1 + 1) % len(self.frames_for_up)
+            image = self.frames_for_up[self.cur_frame1]
             self.image = pygame.transform.scale(
                 image, (image.get_width() * 4,
                         image.get_height() * 4))
+            self.up = True
         elif to_down1:
             if self.right:
-                self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_right)
-                image = self.frames_for_right[self.cur_frame]
-                pygame.time.delay(10)
+                self.cur_frame2 = (self.cur_frame2 + 1) % len(self.frames_for_right)
+                image = self.frames_for_right[self.cur_frame2]
                 self.image = pygame.transform.scale(
                     image, (image.get_width() * 4,
                             image.get_height() * 4))
             else:
-                self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_left)
-                image = self.frames_for_left[self.cur_frame]
-                pygame.time.delay(10)
+                self.cur_frame2 = (self.cur_frame2 + 1) % len(self.frames_for_left)
+                image = self.frames_for_left[self.cur_frame2]
                 self.image = pygame.transform.scale(
                     image, (image.get_width() * 4,
                             image.get_height() * 4))
+            self.up = False
         elif to_left1:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_left)
-            image = self.frames_for_left[self.cur_frame]
-            pygame.time.delay(10)
+            self.cur_frame3 = (self.cur_frame3 + 1) % len(self.frames_for_left)
+            image = self.frames_for_left[self.cur_frame3]
             self.image = pygame.transform.scale(
                 image, (image.get_width() * 4,
                         image.get_height() * 4))
             self.right = False
+            self.up = False
         elif to_right1:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_right)
-            image = self.frames_for_right[self.cur_frame]
-            pygame.time.delay(10)
+            self.cur_frame4 = (self.cur_frame4 + 1) % len(self.frames_for_right)
+            image = self.frames_for_right[self.cur_frame4]
             self.image = pygame.transform.scale(
                 image, (image.get_width() * 4,
                         image.get_height() * 4))
             self.right = True
+            self.up = False
 
 
 class Attack(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y, movement):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, attacks)
         first_pos = pygame.image.load('pictures/gold_attack/gold1.tiff')
         self.image = pygame.transform.scale(
             first_pos, (first_pos.get_width() * 4,
                         first_pos.get_height() * 4))
         self.rect = self.image.get_rect().move(pos_x, pos_y)
         self.cur_frame = 0
+        self.frames = [pygame.transform.rotate(first_pos, 90), pygame.transform.rotate(first_pos, 180),
+                       pygame.transform.rotate(first_pos, 270), pygame.transform.rotate(first_pos, 360)]
         self.frames_for_right = [pygame.image.load('pictures/gold_attack/gold1.tiff'),
                                  pygame.image.load('pictures/gold_attack/gold2.tiff'),
                                  pygame.image.load('pictures/gold_attack/gold3.tiff'),
@@ -108,51 +131,110 @@ class Attack(pygame.sprite.Sprite):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.movement = movement
+        self.damage = 25
 
     def move(self):
         if self.movement == 'up':
-            if self.pos_y > 20:
-                self.pos_y -= self.speed
+            self.pos_y -= self.speed
         if self.movement == 'down':
-            if self.pos_y < height - self.image.get_height() - 20:
-                self.pos_y += self.speed
+            self.pos_y += self.speed
         if self.movement == 'left':
-            if self.pos_x > 20:
-                self.pos_x -= self.speed
+            self.pos_x -= self.speed
         if self.movement == 'right':
-            if self.pos_x < width - self.image.get_width() - 20:
+            self.pos_x += self.speed
+        self.rect = self.image.get_rect().move(self.pos_x, self.pos_y)
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        image = self.frames[self.cur_frame]
+        self.image = pygame.transform.scale(
+            image, (image.get_width() * 4,
+                    image.get_height() * 4))
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(all_sprites, enemies)
+        self.first_pos = pygame.image.load('pictures/enemy/1pos.tiff')
+        self.image = pygame.transform.scale(
+            self.first_pos, (self.first_pos.get_width() * 4,
+                             self.first_pos.get_height() * 4))
+        self.frames_for_right = [pygame.image.load('pictures/enemy/go_right1.tiff'),
+                                 pygame.image.load('pictures/enemy/go_right1.tiff'),
+                                 pygame.image.load('pictures/enemy/go_right2.tiff'),
+                                 pygame.image.load('pictures/enemy/go_right2.tiff'),
+                                 pygame.image.load('pictures/enemy/go_right3.tiff'),
+                                 pygame.image.load('pictures/enemy/go_right3.tiff')]
+        self.frames_for_left = [pygame.transform.flip(im, True, False) for im in self.frames_for_right]
+        self.frames_for_up = [pygame.image.load('pictures/enemy/go_back1.tiff'),
+                              pygame.image.load('pictures/enemy/go_back1.tiff'),
+                              pygame.image.load('pictures/enemy/go_back2.tiff'),
+                              pygame.image.load('pictures/enemy/go_back2.tiff'),
+                              pygame.image.load('pictures/enemy/go_back3.tiff'),
+                              pygame.image.load('pictures/enemy/go_back3.tiff')]
+        self.right = True
+        self.cur_frame1, self.cur_frame2, self.cur_frame3, self.cur_frame4 = 0, 0, 0, 0
+        self.speed = 5
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.HP = 200
+
+    def move(self):
+        if chel.pos_y < self.pos_y:
+            if self.pos_y > 35:
+                self.pos_y -= self.speed
+        if chel.pos_y > self.pos_y:
+            if self.pos_y < height - self.image.get_height() - 15:
+                self.pos_y += self.speed
+        if chel.pos_x < self.pos_x:
+            if self.pos_x > 15:
+                self.pos_x -= self.speed
+        if chel.pos_x > self.pos_x:
+            if self.pos_x < width - self.image.get_width() - 15:
                 self.pos_x += self.speed
         self.rect = self.image.get_rect().move(self.pos_x, self.pos_y)
 
     def update(self):
-        if self.movement == 'up':
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_up)
-            image = self.frames_for_up[self.cur_frame]
-            pygame.time.delay(10)
+        if chel.pos_y < self.pos_y:
+            self.cur_frame1 = (self.cur_frame1 + 1) % len(self.frames_for_up)
+            image = self.frames_for_up[self.cur_frame1]
             self.image = pygame.transform.scale(
                 image, (image.get_width() * 4,
                         image.get_height() * 4))
-        elif self.movement == 'down':
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_down)
-            image = self.frames_for_right[self.cur_frame]
-            pygame.time.delay(10)
+        elif chel.pos_y > self.pos_y:
+            if self.right:
+                self.cur_frame2 = (self.cur_frame2 + 1) % len(self.frames_for_right)
+                image = self.frames_for_right[self.cur_frame2]
+                self.image = pygame.transform.scale(
+                    image, (image.get_width() * 4,
+                            image.get_height() * 4))
+            else:
+                self.cur_frame2 = (self.cur_frame2 + 1) % len(self.frames_for_left)
+                image = self.frames_for_left[self.cur_frame2]
+                self.image = pygame.transform.scale(
+                    image, (image.get_width() * 4,
+                            image.get_height() * 4))
+        elif chel.pos_x < self.pos_x:
+            self.cur_frame3 = (self.cur_frame3 + 1) % len(self.frames_for_left)
+            image = self.frames_for_left[self.cur_frame3]
             self.image = pygame.transform.scale(
                 image, (image.get_width() * 4,
                         image.get_height() * 4))
-        elif self.movement == 'left':
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_left)
-            image = self.frames_for_left[self.cur_frame]
-            pygame.time.delay(10)
+            self.right = False
+        elif chel.pos_x > self.pos_x:
+            self.cur_frame4 = (self.cur_frame4 + 1) % len(self.frames_for_right)
+            image = self.frames_for_right[self.cur_frame4]
             self.image = pygame.transform.scale(
                 image, (image.get_width() * 4,
                         image.get_height() * 4))
-        elif self.movement == 'right':
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames_for_right)
-            image = self.frames_for_right[self.cur_frame]
-            pygame.time.delay(10)
-            self.image = pygame.transform.scale(
-                image, (image.get_width() * 4,
-                        image.get_height() * 4))
+            self.right = True
+
+    def hit(self, damage):
+        self.HP -= damage
+        if self.HP <= 0:
+            enemies.remove(enem)
+            all_sprites.remove(enem)
 
 
 def terminate():
@@ -190,8 +272,8 @@ def start_screen():
 
 
 start_screen()
+clock = pygame.time.Clock()
 chel = Character(100, 100)
-attacks = []
 image = pygame.image.load('pictures/background.png')
 background = pygame.transform.scale(image, (width, height))
 fullscreen_mode = False
@@ -210,17 +292,15 @@ while True:
             if event.key == pygame.K_w:
                 to_up1 = True
             if event.key == pygame.K_LEFT:
-                attack = Attack(chel.pos_x - 10, chel.pos_y, 'left')
-                attacks.append(attack)
+                Attack(chel.pos_x - 10, chel.pos_y + 20, 'left')
             if event.key == pygame.K_RIGHT:
-                attack = Attack(chel.pos_x + 10, chel.pos_y, 'right')
-                attacks.append(attack)
+                Attack(chel.pos_x + 10, chel.pos_y + 20, 'right')
             if event.key == pygame.K_DOWN:
-                attack = Attack(chel.pos_x, chel.pos_y + 10, 'down')
-                attacks.append(attack)
+                Attack(chel.pos_x + 10, chel.pos_y + 10, 'down')
             if event.key == pygame.K_UP:
-                attack = Attack(chel.pos_x, chel.pos_y - 10, 'up')
-                attacks.append(attack)
+                Attack(chel.pos_x + 10, chel.pos_y - 10, 'up')
+            if event.key == pygame.K_q:
+                Enemy(random.randint(500, 700), random.randint(500, 700))
             if event.key == pygame.K_f:
                 fullscreen_mode = not fullscreen_mode
                 if fullscreen_mode:
@@ -230,34 +310,36 @@ while True:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 to_left1 = False
+                chel.cur_frame3 = 0
             if event.key == pygame.K_d:
                 to_right1 = False
+                chel.cur_frame4 = 0
             if event.key == pygame.K_s:
                 to_down1 = False
+                chel.cur_frame2 = 0
             if event.key == pygame.K_w:
+                chel.cur_frame1 = 0
                 to_up1 = False
-    if to_right1:
-        chel.move('right')
-    if to_left1:
-        chel.move('left')
-    if to_down1:
-        chel.move('down')
-    if to_up1:
-        chel.move('up')
-    if attacks:
-        for num, atk in enumerate(attacks):
-            atk.move()
-            if atk.pos_x <= 20:
-                attacks.pop(num)
-            if atk.pos_x >= width - atk.image.get_width() - 20:
-                attacks.pop(num)
-            if atk.pos_y <= 40:
-                attacks.pop(num)
-            if atk.pos_y >= height - atk.image.get_height() - 20:
-                attacks.pop(num)
     screen.fill((255, 255, 255))
-    pygame.time.delay(20)
+    clock.tick(30)
     screen.blit(background, (0, 0))
-    chel.update()
+    if enemies:
+        for enem in enemies:
+            enem.move()
+    if attacks:
+        for atk in attacks:
+            atk.move()
+            if (atk.pos_x <= 10 or atk.pos_x >= width - atk.image.get_width() - 10
+                    or atk.pos_y <= 40 or atk.pos_y >= height - atk.image.get_height() - 10):
+                attacks.remove(atk)
+                all_sprites.remove(atk)
+            if enemies:
+                for enem in enemies:
+                    if atk.rect.colliderect(enem.rect):
+                        enem.hit(atk.damage)
+                        attacks.remove(atk)
+                        all_sprites.remove(atk)
+    chel.move()
+    all_sprites.update()
     all_sprites.draw(screen)
     pygame.display.update()
